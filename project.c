5d4663771a3ae999fd6e3b082767467cb92458eb
deepanshu2003singhal <86514255+deepanshu2003singhal@users.noreply.github.com>
2022-01-11 20:21:31 +0530

    Update project.c

--- project.c
+++ project.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
-    int randomGuess1,randomGuess2,actualNumber=23,i1,i2;
-    printf("Welcome to the game of guessing the number\n You have 5 chances to guess a number ");
+    int randomGuess1,randomGuess2,actualNumber1=33,actualNumber2=13,i1,i2;
+    printf("\n\n\nWelcome to the game of guessing the number\n You have 5 chances to guess a number ");
 //********Player 1
 
 printf("\n First turn is of player 1\n");
@@ -10,9 +10,9 @@ for ( i1 = 4; i1>=0 ; i1--)
 {
     printf("Guess the number:\n");
     scanf("%d",&randomGuess1);
-    if(randomGuess1==actualNumber){printf("Congrats Player1 !,You guess it right..\nNumber of attempts left are %d\n",i1);break;}
-    if(randomGuess1>actualNumber){printf("Try a smaller number.\tNumber of attempts left are %d\n",i1);}
-    if(randomGuess1<actualNumber){printf("Try a greater number.\tNumber of attempts left are %d\n",i1);}
+    if(randomGuess1==actualNumber1){printf("Congrats Player1 !,You guess it right..\nNumber of attempts left are %d\n",i1);break;}
+    if(randomGuess1>actualNumber1){printf("Try a smaller number.\tNumber of attempts left are %d\n",i1);}
+    if(randomGuess1<actualNumber1){printf("Try a greater number.\tNumber of attempts left are %d\n",i1);}
     
 }
 
@@ -25,14 +25,12 @@ for ( i2 = 4; i2>=0 ; i2--)
 {
     printf("Guess the number:\n");
     scanf("%d",&randomGuess2);
-    if(randomGuess2==actualNumber){printf("Congrats Player2 !,You guess it right..\nNumber of attempts left are %d\n",i2);break;}
-    if(randomGuess2>actualNumber){printf("Try a smaller number.\tNumber of attempts left are %d\n",i2);}
-    if(randomGuess2<actualNumber){printf("Try a greater number.\tNumber of attempts left are %d\n",i2);}
+    if(randomGuess2==actualNumber2){printf("Congrats Player2 !,You guess it right..\nNumber of attempts left are %d\n",i2);break;}
+    if(randomGuess2>actualNumber2){printf("Try a smaller number.\tNumber of attempts left are %d\n",i2);}
+    if(randomGuess2<actualNumber2){printf("Try a greater number.\tNumber of attempts left are %d\n",i2);}
     
 }
-int win1=i1-i2;
-int win2=i2-i1;
-int tie=11-12;
+
 
 if(i1==i2){
     printf("Match tie!! Both played nice... Have a rematch?");
